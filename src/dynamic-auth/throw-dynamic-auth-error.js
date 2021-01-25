ff97551945fc4224034e4bd011ec57c92ff81496
Trey Lowerison <19714082+tlowerison@users.noreply.github.com>
2021-01-24 19:34:28 -0800

    removed alteration of error codes

--- src/dynamic-auth/throw-dynamic-auth-error.js
+++ src/dynamic-auth/throw-dynamic-auth-error.js
@@ -2,10 +2,7 @@ import { ApolloError } from 'apollo-server-errors';
 
 export const throwDynamicAuthError = errors => {
   throw new ApolloError(
-    errors.map(({ error: code }) => camelCaseToErrorCodeCase(code)).join(';'),
+    errors.map(({ error }) => error).join(';'),
     errors.map(({ name }) => name.split()).join(';')
   );
 };
-
-const camelCaseToErrorCodeCase = value =>
-  value.replace(/[A-Z]/g, letter => `_${letter}`).toUpperCase();
